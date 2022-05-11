256702e59f0b624e8e7cbd1df289e5f6a33106f3
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-05-11 16:47:30 -0700

    did auth page

--- src/App.js
+++ src/App.js
@@ -17,12 +17,24 @@ import { logout } from './services/fetch-utils';
 
 export default function App() {
   // You'll need to track the user in state
+  const [user, setUser] = useState('');
 
   // add a useEffect to get the user and inject the user object into state on load
+  useEffect(() => {
+    async function fetch() {
+      const user = await getUser();
+      if (user) setUser(user);
+    }
+    
+    fetch();
+  
+  }, []);
 
   async function handleLogout() {
     // call the logout function
+    await logout();
     // clear the user in state
+    setUser('');
   }
 
   return (
@@ -30,11 +42,24 @@ export default function App() {
       <div className='App'>
         <header>
           {/* if there is a user in state, render out a link to the board games list, the create page, and add a button to let the user logout */}
+          {
+            user &&
+            <>
+              <NavLink activeClassName='active-link' to="/board-games">Game List</NavLink>
+              <NavLink activeClassName='active-link' to="/create">Create Page</NavLink>
+              <button onClick={handleLogout}>Logout</button>
+            </>
+          }
+          
         </header>
         <main>
           <Switch>
             <Route exact path="/">
               {/* if there is a user, redirect to the board games list. Otherwise, render the auth page. Note that the AuthPage will need a function called setUser that can set the user state in App.js */}
+              {
+                user
+                  ? <Redirect to="/board-games" />
+                  : <AuthPage setUser={setUser}/>}
             </Route>
             <Route exact path="/board-games">
               {/* if there is a user, render the board games list. Otherwise, redirect to the home route/auth page */}
